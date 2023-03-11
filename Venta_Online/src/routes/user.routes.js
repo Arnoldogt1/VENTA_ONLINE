2166e2a6eaaf66c6c13adab3b7a57a0d1105744e
Arnoldogt1 <109316407+Arnoldogt1@users.noreply.github.com>
2023-03-11 00:36:58 -0600

    Delete Venta_Online directory

--- Venta_Online/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const express = require('express');
-const {Router} = require('express');
-const { usercreate } = require('../controllers/user.controller');
-const api = Router();
-
-api.post('/user_create', usercreate)
-
-module.exports = api;
